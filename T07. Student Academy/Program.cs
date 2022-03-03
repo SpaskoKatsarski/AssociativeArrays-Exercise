c772a625677b0ea3b74d5685f59c3b44aad3bd21
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-03 22:34:55 +0200

    Updated

---
+++ T07. Student Academy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> allStudents = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (allStudents.ContainsKey(studentName))
+                {
+                    allStudents[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    allStudents.Add(studentName, new List<double>{ studentGrade });
+                }
+            }
+
+            Dictionary<string, double> passingStudents = new Dictionary<string, double>();
+
+            foreach (KeyValuePair<string, List<double>> student in allStudents)
+            {
+                double currAverageGrade = student.Value.Average();
+
+                if (currAverageGrade >= 4.50)
+                {
+                    passingStudents.Add(student.Key, currAverageGrade);
+                }
+            }
+
+            foreach (KeyValuePair<string, double> student in passingStudents)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+            }
+        }
+    }
+}
---
+++ T08. Company Users/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] data = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = data[0];
+                string employeeId = data[2];
+
+                if (companies.ContainsKey(companyName))
+                {
+                    if (companies[companyName].Contains(employeeId))
+                    {
+                        // Employee with this ID already exists.
+                        command = Console.ReadLine();
+                        continue;
+                    }
+
+                    companies[companyName].Add(employeeId);
+                }
+                else
+                {
+                    companies.Add(companyName, new List<string>()
+                    {
+                        employeeId
+                    });
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, List<string>> company in companies)
+            {
+                Console.WriteLine(company.Key);
+
+                foreach (string ID in company.Value)
+                {
+                    Console.WriteLine($"-- {ID}");
+                }
+            }
+        }
+    }
+}
