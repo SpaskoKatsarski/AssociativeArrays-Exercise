df416269a07876b954e750ba9a4446d86b946724
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-06 12:00:57 +0200

    Updated

--- T.03-LegendaryWeapong-Retake/Program.cs
+++ T.03-LegendaryWeapong-Retake/Program.cs
@@ -8,185 +8,158 @@ namespace T._03_LegendaryWeapong_Retake
     {
         static void Main(string[] args)
         {
-            const int requiredForLegendary = 250;
-
-            List<string> keyItems = new List<string>
+            List<string> keyMaterials = new List<string>
             {
                 "shards",
                 "fragments",
                 "motes"
             };
 
-            Dictionary<string, int> materials = new Dictionary<string, int>();
+            Dictionary<string, int> keyItems = new Dictionary<string, int>();
 
-            materials.Add("motes", 0);
-            materials.Add("shards", 0);
-            materials.Add("fragments", 0);
+            Dictionary<string, int> junkItems = new Dictionary<string, int>();
 
-            string input = Console.ReadLine();
+            string command = Console.ReadLine();
 
-            while (true)
+            while (!keyItems.Any(i => i.Value >= 250))
             {
-                string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] materialsData = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-                int quantityOne = int.Parse(data[0]);
-                string materialOne = data[1].ToLower();
+                int quantOne = int.Parse(materialsData[0]);
+                string materialOne = materialsData[1].ToLower();
 
-                int quantityTwo = int.Parse(data[2]);
-                string materialTwo = data[3].ToLower();
+                int quantTwo = int.Parse(materialsData[2]);
+                string materialTwo = materialsData[3].ToLower();
 
-                int quantityThree = int.Parse(data[4]);
-                string materialThree = data[5].ToLower();
+                int quantThree = int.Parse(materialsData[4]);
+                string materialThree = materialsData[5].ToLower();
 
-                if (materials.ContainsKey(materialOne))
+                if (keyMaterials.Contains(materialOne))
                 {
-                    materials[materialOne] += quantityOne;
-                }
-                else
-                {
-                    materials.Add(materialOne, quantityOne);
-                }
-
-                if (materials["shards"] >= 250 || materials["fragments"] >= 250 || materials["motes"] >= 250)
-                    break;
-
-                if (materials.ContainsKey(materialTwo))
-                {
-                    materials[materialTwo] += quantityTwo;
-                }
-                else
-                {
-                    materials.Add(materialTwo, quantityTwo);
-                }
-
-                if (materials["shards"] >= 250 || materials["fragments"] >= 250 || materials["motes"] >= 250)
-                    break;
-
-                if (materials.ContainsKey(materialThree))
-                {
-                    materials[materialThree] += quantityThree;
-                }
-                else
-                {
-                    materials.Add(materialThree, quantityThree);
-                }
-
-                if (materials.Any(item => item.Value >= requiredForLegendary && (item.Key == "motes" || item.Key == "shards" || item.Key == "fragments")))
-                {
-                    // We have gathered the materials for a legendary!
-                    break;
-                }
-
-                
-
-                input = Console.ReadLine();
-            }
-
-            string legendaryMaterial = string.Empty;
-
-            foreach (KeyValuePair<string, int> material in materials.Where(m => m.Key == "motes" || m.Key == "shards" || m.Key == "fragments"))
-            {
-                if (material.Value >= requiredForLegendary)
-                {
-                    if (material.Key == "shards")
+                    if (keyItems.ContainsKey(materialOne))
                     {
-                        Console.WriteLine("Shadowmourne obtained!");
-                        legendaryMaterial = "shards";
-                        break;
-                    }
-                    else if (material.Key == "fragments")
-                    {
-                        Console.WriteLine("Valanyr obtained!");
-                        legendaryMaterial = "fragments";
-                        break;
+                        keyItems[materialOne] += quantOne;
                     }
                     else
                     {
-                        Console.WriteLine("Dragonwrath obtained!");
-                        legendaryMaterial = "motes";
-                        break;
+                        keyItems.Add(materialOne, quantOne);
                     }
                 }
+                else
+                {
+                    if (junkItems.ContainsKey(materialOne))
+                    {
+                        junkItems[materialOne] += quantOne;
+                    }
+                    else
+                    {
+                        junkItems.Add(materialOne, quantOne);
+                    }
+                }
+
+                if (keyItems.Any(i => i.Value >= 250))
+                {
+                    break;
+                }
+
+                if (keyMaterials.Contains(materialTwo))
+                {
+                    if (keyItems.ContainsKey(materialTwo))
+                    {
+                        keyItems[materialTwo] += quantTwo;
+                    }
+                    else
+                    {
+                        keyItems.Add(materialTwo, quantTwo);
+                    }
+                }
+                else
+                {
+                    if (junkItems.ContainsKey(materialTwo))
+                    {
+                        junkItems[materialTwo] += quantTwo;
+                    }
+                    else
+                    {
+                        junkItems.Add(materialTwo, quantTwo);
+                    }
+                }
+
+                if (keyItems.Any(i => i.Value >= 250))
+                {
+                    break;
+                }
+
+                if (keyMaterials.Contains(materialThree))
+                {
+                    if (keyItems.ContainsKey(materialThree))
+                    {
+                        keyItems[materialThree] += quantThree;
+                    }
+                    else
+                    {
+                        keyItems.Add(materialThree, quantThree);
+                    }
+                }
+                else
+                {
+                    if (junkItems.ContainsKey(materialThree))
+                    {
+                        junkItems[materialThree] += quantThree;
+                    }
+                    else
+                    {
+                        junkItems.Add(materialThree, quantThree);
+                    }
+                }
+
+                if (keyItems.Any(i => i.Value >= 250))
+                {
+                    break;
+                }
+
+                command = Console.ReadLine();
             }
 
-            materials[legendaryMaterial] -= requiredForLegendary;
+            // When the loop ends we have enough material for legendary.
+            string firstGatheredKeyMaterial = string.Empty;
 
-            foreach (KeyValuePair<string, int> material in materials)
+            foreach (KeyValuePair<string, int> item in keyItems)
             {
-                if (keyItems.Contains(material.Key))
+                if (item.Value >= 250)
                 {
-                    // Printing only the key items:
-                    Console.WriteLine($"{material.Key}: {material.Value}");
+                    firstGatheredKeyMaterial = item.Key;
+                    break;
                 }
             }
 
-            foreach (KeyValuePair<string, int> junkMaterial in materials.Where(i => i.Key != keyItems[0] && i.Key != keyItems[1] && i.Key != keyItems[2]))
+            string legendaryItem = string.Empty;
+
+            if (firstGatheredKeyMaterial == "shards")
             {
-                Console.WriteLine($"{junkMaterial.Key}: {junkMaterial.Value}");
+                legendaryItem = "Shadowmourne";
+            }
+            else if (firstGatheredKeyMaterial == "fragments")
+            {
+                legendaryItem = "Valanyr";
+            }
+            else
+            {
+                legendaryItem = "Dragonwrath";
             }
 
-            //string legendaryItem = String.Empty;
-            //Dictionary<string, int> materials = new Dictionary<string, int>();
-            //materials.Add("motes", 0);
-            //materials.Add("shards", 0);
-            //materials.Add("fragments", 0);
-            //Dictionary<string, int> junk = new Dictionary<string, int>();
+            keyItems[firstGatheredKeyMaterial] -= 250;
+            Console.WriteLine($"{legendaryItem} obtained!");
 
-            //while (materials["motes"] < 250 && materials["fragments"] < 250 && materials["shards"] < 250)
-            //{
-            //    string input = Console.ReadLine().ToLower();
-            //    string[] inputTokens = input.Split(" ");
-            //    for (int i = 0; i < inputTokens.Length; i += 2)
-            //    {
-            //        int quantity = int.Parse(inputTokens[i]);
-            //        string material = inputTokens[i + 1];
+            foreach (KeyValuePair<string, int> keyMat in keyItems)
+            {
+                Console.WriteLine($"{keyMat.Key}: {keyMat.Value}");
+            }
 
-            //        switch (material)
-            //        {
-            //            case "shards":
-            //            case "fragments":
-            //            case "motes":
-            //                materials[material] += quantity;
-            //                break;
-            //            default:
-            //                if (!junk.ContainsKey(material))
-            //                    junk.Add(material, 0);
-            //                junk[material] += quantity;
-            //                break;
-            //        }
-
-            //        if (materials["shards"] >= 250 || materials["fragments"] >= 250 || materials["motes"] >= 250)
-            //            break;
-            //    }
-            //}
-
-            //if (materials["shards"] >= 250)
-            //{
-            //    legendaryItem = "Shadowmourne";
-            //    materials["shards"] -= 250;
-            //}
-            //else if (materials["fragments"] >= 250)
-            //{
-            //    legendaryItem = "Valanyr";
-            //    materials["fragments"] -= 250;
-            //}
-            //else
-            //{
-            //    legendaryItem = "Dragonwrath";
-            //    materials["motes"] -= 250;
-            //}
-
-            //Console.WriteLine($"{legendaryItem} obtained!");
-
-            //foreach (var item in materials.OrderByDescending(entry => entry.Value).ThenBy(entry => entry.Key))
-            //{
-            //    Console.WriteLine($"{item.Key}: {item.Value}");
-            //}
-
-            //foreach (var item in junk.OrderBy(entry => entry.Key))
-            //{
-            //    Console.WriteLine($"{item.Key}: {item.Value}");
-            //}
+            foreach (KeyValuePair<string, int> junk in junkItems)
+            {
+                Console.WriteLine($"{junk.Key}: {junk.Value}");
+            }
         }
     }
 }
